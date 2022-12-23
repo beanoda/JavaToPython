27809b5c57a2f344e02d5e38f893c0aed43238f3
beanoda <sumgar3228@gmail.com>
2022-12-23 00:22:55 -0600

    Created a function named 'tokenize' used for testing out the lexer

--- main.py
+++ main.py
@@ -2,6 +2,26 @@
 Main module which interacts with the source directory
 """
 
+from src import lexer
+from ply import lex
+
+
+def tokenize(input_data: str):
+    """ Tests the lexer given input """
+
+    built_lexer = lex.lex(module=lexer)
+    built_lexer.input(input_data)
+
+    # Get tokens
+    while True:
+        token = built_lexer.token()
+        if token is None:
+            break
+
+        print(token)
+
 
 if __name__ == "__main__":
-    pass
+    while True:
+        data = input(">>> ")
+        tokenize(data)
