77df29ce2306819e1f2217b202bb9059f21cde0e
beanoda <sumgar3228@gmail.com>
2022-12-23 11:47:42 -0600

    Created a basic implementation in the lexer for floats
    Java has doubles (ex: 1.2) and floats (ex 1.2f) but in Python,
    these are seen as the same thing. I've decided to implement the java
    float later because I am lazy.

--- src/lexer.py
+++ src/lexer.py
@@ -10,8 +10,14 @@ all the code into a single file
 
 
 tokens = (
-    "INT",  # INT short for Integer
+    "INT",  # INT short for Integer,
+    "FLOAT",
 )
+
+# Java's doubles are just floats in Python
+# Java's floats are difficult because they have an f appended to them
+# ill implement Java's floats later
+t_FLOAT = r"\d+\.\d+"
 
 # It's important this is placed after FLOAT
 t_INT = r"\d+"
