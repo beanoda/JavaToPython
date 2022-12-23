e5fd3a5e636882b43cc50bd1e7f7ab2ab60718b0
beanoda <sumgar3228@gmail.com>
2022-12-23 00:10:43 -0600

    Created basic lexer.py; For now, all it has is a descriptive docstring

---
+++ src/lexer.py
@@ -0,0 +1,12 @@
+"""
+This module contains the lexer.
+
+It's important that the tuple of tokens be imported where it is needed.
+Ply is weird and requires it.
+
+This lexer is also an alternative specification opposed to piling
+all the code into a single file
+"""
+
+
+tokens = ()
