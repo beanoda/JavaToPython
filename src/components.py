8c89567b2c60054d0353f5b7d038668bd77b0622
beanoda <sumgar3228@gmail.com>
2022-12-24 11:15:27 -0600

    Components contains classes that aid interpretation
    Every class inside of the file represent different parts of the Python
    language. For example, integer has the Integer class and function has
    the Function class. Terms has a Term class. These are necessary because
    they give the code more readability.

---
+++ src/components.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+"""
+This module contains classes that represent parts of
+the Python language
+
+For instance, there is a component (class) for an Integer,
+for a Float, and for a Function.
+"""
+
+
+from dataclasses import dataclass
+from typing import *
+import enum
+
+
+@dataclass
+class Integer(object):
+    value: int
+
+
+@dataclass
+class Float(object):
+    value: float
+
+
+@dataclass
+class Factor(object):
+    value: Union[Integer, Float]
+
+
+@dataclass
+class Term(object):
+    left: Factor
+    operator: Operator
+    right: Factor
+
+
+@dataclass
+class Expression(object):
+    left: Term
+    operator: Operator
+    right: Term
+
+
+@dataclass
+class Operator(enum.Enum):
+    PLUS = "+"
+    MINUS = "-"
+    MULTIPLY = "*"
+    DIVIDE = "/"
