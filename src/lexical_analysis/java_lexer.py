ca0feca26d5f4a4a61aa55998f9683dc224ba315
beanoda <sumgar3228@gmail.com>
2022-12-31 22:58:46 -0600

    Unicode and LineTerminators regex are functional

--- src/lexical_analysis/java_lexer.py
+++ src/lexical_analysis/java_lexer.py
@@ -3,7 +3,8 @@ from typing import Tuple
 
 tokens = []
 
-# Constants used in regex and tokens
+# Constants used in regex and tokens. If it dont start w/ t, it's a
+# constant
 HEX_DIGIT = r"[0-9a-fA-F]"
 
 # UnicodeEscape encapsulates UnicodeMarker because it is a simple rule
@@ -12,3 +13,7 @@ UNICODE_ESCAPE = r"\\u+[0-9a-fA-F]{4}"
 # It says any unicode character. So non-specific that it hurts me
 RAW_INPUT_CHARACTER = r"[\S\s]"
 UNICODE_INPUT_CHARACTER = fr"{UNICODE_ESCAPE}|{RAW_INPUT_CHARACTER}"
+
+# Matches LF, CR, or CRLF
+LINE_TERMINATOR = r"\n|\r|(\r\n)"
+INPUT_CHARACTER = fr"{UNICODE_INPUT_CHARACTER}"
